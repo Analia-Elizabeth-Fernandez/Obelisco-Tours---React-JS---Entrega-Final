158e730c36501d0bd4b108860a3a588609ea9e3f
Analia-Elizabeth-Fernandez <anfer510@gmail.com>
2025-12-05 09:55:20 -0300

    Update Header.jsx
    Carga de logo

--- src/components/estaticos/Header.jsx
+++ src/components/estaticos/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header>
       <nav>
         <ul>
-          <img src="public/img/obelisco.png"  alt="logo" width="50" height="50"></img>
+          <img src="/img/obelisco.png"  alt="logo" width="50" height="50"></img>
           <li><Link to='/' className='link'>Bienvenidos</Link></li>
           <li><Link to='/NuestrosTours' className='link'>Nuestros Tours</Link></li>
           <li><Link to='/PreguntasFrecuentes' className='link'>Preguntas Frecuentes</Link></li>
@@ -22,5 +22,6 @@ const Header = () => {
     </header>
   )
 }
+
 
 export default Header
