ea3549eddfdd28fc0a966f1f6f88a46f09b104d2
Analia-Elizabeth-Fernandez <anfer510@gmail.com>
2025-12-05 10:18:23 -0300

    Update Header.jsx

--- src/components/estaticos/Header.jsx
+++ src/components/estaticos/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <img src="/img/obelisco.png" alt="logo" width="50" height="50" />
+        <img src="/obelisco.png" alt="logo" width="50" height="50" />
         <ul> 
           <li><Link to='/' className='link'>Bienvenidos</Link></li>
           <li><Link to='/NuestrosTours' className='link'>Nuestros Tours</Link></li>
@@ -25,5 +25,6 @@ const Header = () => {
 
 
 export default Header
+
 
 
